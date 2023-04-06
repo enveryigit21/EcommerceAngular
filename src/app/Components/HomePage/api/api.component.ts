57419625465764bc7790f0ec9d4b4323b6e01385
enveryigit21 <enver21667@gmail.com>
2023-04-06 18:14:16 +0300

    commit 2

--- src/app/Components/HomePage/api/api.component.ts
+++ src/app/Components/HomePage/api/api.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Products } from 'src/app/model';
 import { ProductService } from 'src/app/services/product.service';
+import { CategoryComponent } from '../categorys/category/category.component';
 
 @Component({
   selector: 'app-api',
@@ -7,20 +9,30 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./api.component.css']
 })
 export class ApiComponent implements OnInit {
+  @ViewChild(CategoryComponent) CategoryComponent!: CategoryComponent;
 
-   products: any = []
-   @Input() filteredProducts: any = [];
+  products: any = [];
+  filteredProducts: Products[] = [];
 
-   constructor(private service:ProductService) {}
+  constructor(private service: ProductService) {}
 
-   ngOnInit(): void {
-    this.getProductsList() ;
-   }
+  ngOnInit(): void {
+    this.getProductsList();
+  }
 
-   getProductsList() {
+  getProductsList() {
     this.service.getProducts().subscribe(res => {
       this.products = res;
+      this.filteredProducts = this.products;
     })
   }
 
+  filterProducts() {
+    this.filteredProducts = this.products.filter((p:any) => p.stock > 0);
+    if (this.filteredProducts.length > 0) {
+      this.products = this.filteredProducts;
+    } else if (this.filteredProducts.length === 0) {
+      this.products = this.products;
+    }
+  }
 }
--- src/app/Components/HomePage/categorys/category/category.component.ts
+++ src/app/Components/HomePage/categorys/category/category.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { map } from 'rxjs/operators';
+import { Products } from 'src/app/model';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,33 +10,48 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
+  
   priceFilterForm!: FormGroup;
-  @Output() filteredProductsEvent = new EventEmitter<any>();
-
+  filteredProducts: any = [];
+  @Output() filteredProductsEvent: EventEmitter<any> = new EventEmitter();
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.getForm();
+    this.getProducts();
   }
 
-  getForm(){
+  getProducts(){
+    this.productService.getProducts().subscribe(res => {
+      console.log(res);
+      this.filteredProducts = res;
+    });
+  }
+
+  getForm() {
     this.priceFilterForm = new FormGroup({
       minPrice: new FormControl(''),
-      maxPrice : new FormControl('')
-    })
+      maxPrice: new FormControl('')
+    });
   }
 
   filterProducts() {
-    const minPrice = this.priceFilterForm.get('minPrice')?.value;
-    const maxPrice = this.priceFilterForm.get('maxPrice')?.value;
-
+    const minPrice = this.priceFilterForm.controls['minPrice'].value;
+    const maxPrice = this.priceFilterForm.controls['maxPrice'].value;
+  
     this.productService.getProducts().pipe(
-      map(products => products.filter((product: any) => {
-        return product.price >= minPrice && product.price <= maxPrice;
-      }))
+      map(products => {
+        return products.filter(product => {
+          const price = product.price;
+          return (minPrice ? price >= minPrice : true) && (maxPrice ? price <= maxPrice : true);
+        });
+      })
     ).subscribe(filteredProducts => {
-      this.filteredProductsEvent.emit(filteredProducts);
+      console.log(filteredProducts);
+      this.filteredProducts = filteredProducts;
+      this.filteredProductsEvent.emit(this.filteredProducts);
     });
   }
+  
+
 }
